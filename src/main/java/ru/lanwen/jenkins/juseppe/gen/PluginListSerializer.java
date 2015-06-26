ace21c5d3c96268f8717b14a8a02535916132404
MerkushevKirill <lanwen@yandex-team.ru>
2015-06-26 15:34:15 +0300

    fix - plugin name and version. Throw exc on bad name or version

--- src/main/java/ru/lanwen/jenkins/juseppe/gen/PluginListSerializer.java
+++ src/main/java/ru/lanwen/jenkins/juseppe/gen/PluginListSerializer.java
@@ -10,9 +10,6 @@ import ru.lanwen.jenkins.juseppe.beans.Plugin;
 import java.lang.reflect.Type;
 import java.util.List;
 
-import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
-import static org.apache.commons.lang3.StringUtils.trimToEmpty;
-
 /**
  * User: lanwen
  * Date: 28.01.15
@@ -28,12 +25,7 @@ public class PluginListSerializer implements JsonSerializer<List<Plugin>> {
         JsonObject jsonObject = new JsonObject();
 
         for (Plugin plugin : src) {
-            jsonObject.add(
-                    defaultIfEmpty(
-                            plugin.getName(), 
-                            trimToEmpty(plugin.getTitle()).replace(" ", "-").toLowerCase()
-                    ),
-                    context.serialize(plugin));
+            jsonObject.add(plugin.getName(), context.serialize(plugin));
         }
 
         return jsonObject;
