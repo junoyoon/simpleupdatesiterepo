48ca2ef02bd6dd7648e33971615fe76574947d39
Kirill Merkushev <lanwen@yandex.ru>
2016-06-03 13:46:14 +0300

    log status

--- juseppe-cli/src/test/java/ru/lanwen/jenkins/juseppe/cli/ServeCommandTest.java
+++ juseppe-cli/src/test/java/ru/lanwen/jenkins/juseppe/cli/ServeCommandTest.java
@@ -1,7 +1,7 @@
 package ru.lanwen.jenkins.juseppe.cli;
 
 
-import org.junit.Rule;
+import org.junit.ClassRule;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.given;
@@ -11,27 +11,36 @@ import static com.jayway.restassured.RestAssured.given;
  */
 public class ServeCommandTest {
 
-    @Rule
-    public ServeRule srv = new ServeRule();
-    
+    @ClassRule
+    public static ServeRule srv = new ServeRule();
+
     @Test(timeout = 10000)
     public void shouldFetchUpdateCenter() throws Exception {
         given()
                 .baseUri(srv.uri().toString())
-                .when().get("update-center.json").then().assertThat().statusCode(200);
+                .log().all()
+                .expect()
+                .log().status()
+                .get("update-center.json").then().assertThat().statusCode(200);
     }
-    
+
     @Test(timeout = 10000)
     public void shouldFetchHpi() throws Exception {
         given()
                 .baseUri(srv.uri().toString())
-                .when().get("clang-scanbuild-plugin.hpi").then().assertThat().statusCode(200);
+                .log().all()
+                .expect()
+                .log().status()
+                .get("clang-scanbuild-plugin.hpi").then().assertThat().statusCode(200);
     }
-    
+
     @Test(timeout = 10000)
     public void shouldFetchJpi() throws Exception {
         given()
                 .baseUri(srv.uri().toString())
-                .when().get("sample-pipeline-dsl-ext-plugin-0.1.0.jpi").then().assertThat().statusCode(200);
+                .log().all()
+                .expect()
+                .log().status()
+                .get("sample-pipeline-dsl-ext-plugin-0.1.0.jpi").then().assertThat().statusCode(200);
     }
 }
